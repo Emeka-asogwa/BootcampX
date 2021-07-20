2e850b49d646d027a992000a92e56b41b824cf7c
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-07-20 02:01:16 +0000

    ✍🏽 Add query 2 on joins

--- 2_queries_joins/0_joins.sql
+++ 2_queries_joins/0_joins.sql
@@ -9,6 +9,9 @@
 -- ############################################### 
 -- total amount of time that all students from a specific cohort have spent on all assignments.
 -- ###############################################
-SELECT SUM(assignment_submissions.duration) AS total_duration
-FROM assignment_submissions JOIN students ON students.id = student_id
-WHERE submission_date =FEB12;
+SELECT cohorts.name AS cohort, sum(duration) AS total_duration
+FROM assignment_submissions
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12'
+GROUP BY cohorts.name;
