6309f28a021143991e436fbea613190383356ee4
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-07-20 01:00:56 +0000

    ✍🏽 Add docs folder with ERD pix

---
+++ 2_queries_joins/0_joins.sql
@@ -0,0 +1,14 @@
+-- ############################################### 
+-- total amount of time that a student has spent on all assignments.
+-- ############################################### 
+-- SELECT sum(assignment_submissions.duration) as total_duration
+-- FROM assignment_submissions
+--   JOIN students ON students.id = student_id
+-- WHERE students.name = 'Ibrahim Schimmel';
+
+-- ############################################### 
+-- total amount of time that all students from a specific cohort have spent on all assignments.
+-- ###############################################
+SELECT SUM(assignment_submissions.duration) AS total_duration
+FROM assignment_submissions JOIN students ON students.id = student_id
+WHERE submission_date =FEB12;
