01056e42f1e194c35812447df1f20cb5e7534b4b
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-07-21 03:17:07 +0000

    ✍🏽 Nested queries

--- 3_queries_group-by/total_number_of_assignments.sql
+++ 3_queries_group-by/total_number_of_assignments.sql
@@ -1,49 +1,49 @@
 -- -- ############################################### 
 -- -- total number of assignments for each day of bootcamp.
 -- -- ############################################### 
--- SELECT day, COUNT(day) AS total_assignments
--- FROM assignments
--- GROUP BY day
--- ORDER BY day;
+SELECT day, COUNT(day) AS total_assignments
+FROM assignments
+GROUP BY day
+ORDER BY day;
 
 -- -- ############################################### 
 -- --  total assignments is greater than or equal to 10.
 -- -- ############################################### 
--- SELECT day, COUNT(day) AS total_assignments
--- FROM assignments
--- GROUP BY day
--- HAVING COUNT(day) >= 10
--- ORDER BY day;
+SELECT day, COUNT(day) AS total_assignments
+FROM assignments
+GROUP BY day
+HAVING COUNT(day) >= 10
+ORDER BY day;
 
 -- ############################################### 
 --  all cohorts with 18 or more students.
 -- ############################################### 
--- SELECT cohorts.name as cohort_name, count(students) AS student_count
--- FROM cohorts
---   JOIN students ON cohorts.id = cohort_id
--- GROUP BY cohort_name
--- HAVING count(students) >= 18
--- ORDER BY student_count;
+SELECT cohorts.name as cohort_name, count(students) AS student_count
+FROM cohorts
+  JOIN students ON cohorts.id = cohort_id
+GROUP BY cohort_name
+HAVING count(students) >= 18
+ORDER BY student_count;
 
 -- -- #######################################
 -- -- total number of assignment submissions for each cohort
 -- -- ############################################### 
--- SELECT cohorts.name as cohort, count(assignment_submissions) as total_submissions
--- FROM assignment_submissions
---   JOIN students ON students.id = student_id
---   JOIN cohorts ON cohorts.id = cohort_id
--- GROUP BY cohorts.name
--- ORDER BY total_submissions DESC;
+SELECT cohorts.name as cohort, count(assignment_submissions) as total_submissions
+FROM assignment_submissions
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
 
 -- -- #######################################
 -- -- currently enrolled students' average assignment completion time.
 -- -- ############################################### 
--- SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
--- FROM students
---   JOIN assignment_submissions ON student_id = students.id
--- WHERE end_date IS NULL
--- GROUP BY student
--- ORDER BY average_assignment_duration DESC;
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+  JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
 
 -- #######################################
 -- students who's average time it takes to complete an assignment is less than the average estimated time it takes to complete an assignment.
