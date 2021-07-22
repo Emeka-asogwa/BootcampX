ae267c0d5e4a6b09136cc735a36af2ae9329e049
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-07-22 19:02:52 +0000

    ✍🏽 parameterize query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,3 +1,26 @@
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   user: 'vagrant',
+//   password: '123',
+//   host: 'localhost',
+//   database: 'bootcampx'
+// });
+// // Querying the Database
+// const queryString = `
+// SELECT students.id AS student_id, students.name, cohorts.name AS cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `;
+// pool.query(queryString)
+//   .then(res => {
+//     res.rows.forEach(user => {
+//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//     })
+//   }).catch(err => console.error('query error', err.stack));
+
+
 const { Pool } = require('pg');
 const pool = new Pool({
   user: 'vagrant',
@@ -5,15 +28,21 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
+// const cohortName = process.argv.slice(2)[0];
+// const limit = process.argv.slice(2)[1] || 5;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 // Querying the Database
-pool.query(`
+const queryString = `
 SELECT students.id AS student_id, students.name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,3 +1,26 @@
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   user: 'vagrant',
+//   password: '123',
+//   host: 'localhost',
+//   database: 'bootcampx'
+// });
+
+// const queryString = `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;`
+
+// pool.query(queryString)
+//   .then(res => {
+//     res.rows.forEach(row => {
+//       console.log(`${row.cohort}: ${row.teacher}`);
+//     })
+//   }).catch(err => console.error('query error', err.stack));
+
 const { Pool } = require('pg');
 const pool = new Pool({
   user: 'vagrant',
@@ -6,15 +29,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const query = `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+// Parameterized Query
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`${cohortName}`];
+const queryString = `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;`
+WHERE cohorts.name = $1
+ORDER BY teacher;`;
 
-pool.query(query)
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
